df6ebed04984c03044a0365a3165a6e644c6cd55
Tom Timmerman <info@tomtimmerman.nl>
2017-10-13 05:54:04 +0200

    some formating

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 TODO
 - 
 
-- bulkrender controller renderd nu de hele queue in 1 keer, deze nog even chunken
 - handlebars helpers uit de definitions file halen en in losse file zetten
 
 
--- services/render.process.service.js
+++ services/render.process.service.js
@@ -270,13 +270,11 @@ class RenderProcessService {
 
             self.renderQueue.get() // get all items in render queue
                 .then((queue) => {
-                    const chunkSize = 2; // set chunk size, number of templates that are rendered async at the same time
+                    const chunkSize = 3; // set chunk size, number of templates that are rendered async at the same time
                     const chunkedQueue = _.chunk(queue, chunkSize); // chunk render queue
 
                     // iterate chunks serially
-                    Promise.each(chunkedQueue, (chunk, i) => {
-                        return self._renderQueueChunk(chunk, (i + 1), chunkedQueue.length, correlationId);
-                    })
+                    Promise.each(chunkedQueue, (chunk, i) => { return self._renderQueueChunk(chunk, (i + 1), chunkedQueue.length, correlationId); })
                         .then(() => {
                             logger.info('Rendered all templates in render queue', correlationId);
                             resolve({});
