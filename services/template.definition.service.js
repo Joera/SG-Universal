94724b1d4026f8b2c3f3c2aace987bd13e4368d4
Tom Ootes <tomootes@gmail.com>
2017-10-26 15:28:45 +0200

    changed persistence

--- services/template.definition.service.js
+++ services/template.definition.service.js
@@ -32,8 +32,6 @@ class TemplateDefinitionService {
                     // data.type should match the name property of a template definition
                     let definition = _.find(templateDefinitions, (td) => { return td && td.name === templateName; });
 
-                    console.log(templateName);
-
                     if(definition) { // return template definition
                         logger.info('Found template definition for type: ' + templateName, correlationId);
                         resolve(definition);
