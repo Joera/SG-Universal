8a6dd231d88eb2a9032b61a140602b298811b65a
Tom Ootes <tomootes@gmail.com>
2017-11-15 14:48:14 +0100

    configs added

---
+++ config/development/index.js
@@ -0,0 +1,14 @@
+const devConfig = {
+    env: 'development',
+    dist: 'dist',
+    db: 'mongodb://localhost/alv-test',
+    port: 4040,
+    wordpresUrl: 'http://wp.avl.nl',
+    algoliaIndexNamePrefix: 'AVL_core',
+    algoliaApplicationId: 'RLZJT7BFZT',
+    algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
+    baseUrl: 'http://sg.avl.nl',
+    templateNameKey: 'type'
+};
+
+module.exports = devConfig;
---
+++ config/production/index.js
@@ -0,0 +1,14 @@
+const devConfig = {
+    env: 'production',
+    dist: '/var/www/html',
+    db: 'mongodb://localhost/sg-core',
+    port: 8080,
+    wordpresUrl: 'http://46.101.100.111/cms',
+    algoliaIndexNamePrefix: 'AVL_core',
+    algoliaApplicationId: 'RLZJT7BFZT',
+    algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
+    baseUrl: 'http://46.101.100.111',
+    templateNameKey: 'type'
+};
+
+module.exports = devConfig;
