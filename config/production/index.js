92ae31aeed89a59add255f30cd1de8cc81cf3220
Joera <joeramulders@gmail.com>
2017-11-17 13:48:42 +0100

    removin config develeopment js from repo

---
+++ config/production/index.js
@@ -0,0 +1,14 @@
+const devConfig = {
+    env: 'production',
+    dist: '/var/www/html',
+    db: 'mongodb://localhost/amstelveenlijn',
+    port: 8080,
+    wordpresUrl: 'http://46.101.100.111',
+    algoliaIndexNamePrefix: 'AVL_core',
+    algoliaApplicationId: 'RLZJT7BFZT',
+    algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
+    baseUrl: 'http://46.101.100.111',
+    templateNameKey: 'type'
+};
+
+module.exports = devConfig;
