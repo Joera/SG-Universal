6925e83261c921ccd77767dcb0a1cd6c83cdfda9
Tom Ootes <tomootes@gmail.com>
2017-11-21 12:14:32 +0100

    removed logs

--- services/template.service.js
+++ services/template.service.js
@@ -172,7 +172,6 @@ class TemplateService {
      * @param correlationId             id for correlation through the process chain
      */
     render(name, template, data, correlationId) {
-        console.log('rendrin?');
         const self = this;
         return new Promise((resolve, reject) => {
             self._registerHelpers(correlationId) // register helper functions
