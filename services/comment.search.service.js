482724c3c076699cd293bf39f5b93290d93d9d5b
Joera <joeramulders@gmail.com>
2018-01-20 22:01:32 +0100

    comment sarch service

--- services/comment.search.service.js
+++ services/comment.search.service.js
@@ -69,7 +69,7 @@ class CommentSearchService {
         if(data.comments && data.comments.length > 0) {
 
 
-            return Promise.all(data.threads.map(function (thread) {
+            return Promise.all(data.comments.map(function (thread) {
 
                 return new Promise(function (resolve, reject) {
 
@@ -103,7 +103,7 @@ class CommentSearchService {
                 return self.templateService.render('search-snippet','thread-search-snippet.handlebars', thread.snippetData, correlationId);
             }))
 
-        } 
+        }
     }
 
     _uploadSnippets(data,correlationId){
