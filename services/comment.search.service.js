e3d62822e074f2a1a31c2e2142b5fcccfe43e259
Joera <joeramulders@gmail.com>
2018-02-07 11:15:29 +0100

    debugging actuele werkzaamheden

--- services/comment.search.service.js
+++ services/comment.search.service.js
@@ -40,18 +40,21 @@ class CommentSearchService {
 
                         data.threads = [];
 
-                        return new Promise((res, rej) => {
-                            for (let i = 0; i < threads.length; i++) {
-                                let threadObject = {};
-                                threadObject.objectID = threads[i].id;
-                                threadObject.date = threads[i].date;
-                                threadObject.type = 'comments';
-                                threadObject.snippetData = threads[i];
-                                threadObject.comments = threads[i].comments;
-                                data.threads.push(threadObject);
-                            }
-                            res(data);
-                        });
+                        if(threads !== null) {
+
+                            return new Promise((res, rej) => {
+                                for (let i = 0; i < threads.length; i++) {
+                                    let threadObject = {};
+                                    threadObject.objectID = threads[i].id;
+                                    threadObject.date = threads[i].date;
+                                    threadObject.type = 'comments';
+                                    threadObject.snippetData = threads[i];
+                                    threadObject.comments = threads[i].comments;
+                                    data.threads.push(threadObject);
+                                }
+                                res(data);
+                            });
+                        }
                     })
                     .then((data) => { return self._renderSnippets(data,correlationId); })
                     .then((snippets) => {
