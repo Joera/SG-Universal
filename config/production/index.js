5b8c001c50db9da2306082133f5ca7d13cb4955c
Joera <joeramulders@gmail.com>
2018-02-12 15:27:56 +0100

    a

--- config/production/index.js
+++ config/production/index.js
@@ -7,7 +7,7 @@ const devConfig = {
     algoliaIndexNamePrefix: 'AVL_core',
     algoliaApplicationId: 'RLZJT7BFZT',
     algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
-    baseUrl: 'http://46.101.100.111',
+    baseUrl: 'http://amstelveenlijn.nl',
     templateNameKey: 'type'
 };
 
