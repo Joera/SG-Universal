48c3eab20ee2d063a75b30b8f2c0f403b3e9e780
Joera <joeramulders@gmail.com>
2018-02-27 13:43:36 +0100

    fb card image

--- connectors/wordpress.connector.js
+++ connectors/wordpress.connector.js
@@ -29,8 +29,10 @@ class WordpressConnector {
 
 			let url = config.wordpressUrl + '/api/get_posts/?count=-1';
             // send http request
+            logger.info(url);
             requestify.get(url,{
-                redirect: true
+                redirect: true,
+                timeout: 120000
             }).then((response) => {
                     logger.info('Received posts from wordpress', correlationId);
                     resolve(response.getBody().posts); // only return the posts of api response
