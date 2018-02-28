d317e721d7c5c5e507c8d2a1864ffa55fcf4c2e8
Tom Ootes <tomootes@gmail.com>
2018-02-28 14:35:46 +0100

    added endpoint for tests

--- routes/test.route.js
+++ routes/test.route.js
@@ -10,7 +10,7 @@ router.route('/')
     // POST /api/blog - save new blog post
     .post(function(req, res){
         res.status(200); // set http status code for response
-        res.json({message: config.baseUrl + ':8080/api is up, test succeeded.'}); // send response body
+        res.json({message: config.baseUrl + ':' + config.port + '/api is up, test succeeded.'}); // send response body
     })
 
 module.exports = router;
