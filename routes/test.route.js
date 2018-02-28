bed857308874ebb4838ff444f51735a96de58d00
Tom Ootes <tomootes@gmail.com>
2018-02-28 14:48:44 +0100

    end point is now get

--- routes/test.route.js
+++ routes/test.route.js
@@ -8,7 +8,7 @@ const config = require('../config');
 // CRUD routes
 router.route('/')
     // POST /api/blog - save new blog post
-    .post(function(req, res){
+    .get(function(req, res){
         res.status(200); // set http status code for response
         res.json({message: config.baseUrl + ':' + config.port + '/api is up, test succeeded.'}); // send response body
     })
