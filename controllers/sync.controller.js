9230a531a89b9969d280414b8d03771307f42455
Joera <joeramulders@gmail.com>
2018-03-20 14:25:49 +0100

    aa

--- controllers/sync.controller.js
+++ controllers/sync.controller.js
@@ -69,8 +69,7 @@ class SyncController {
                 });
         });
     }
-
-
+    
     /**
      * Delete pages in pages array
      * @param pages                     array of pages that need to be deleted
