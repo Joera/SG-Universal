9a5980d6be7dc901d8626de395b18ded9c122ae9
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-05-25 16:20:03 +0200

    extra renders uitgezet

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -238,10 +238,10 @@ class PageController {
 
                 // update search
                 // only update search if search snippet is rendered. if searchSnippet property on data object is undefined or an empty string search will NOT be updated
-                .then(() => { return self.searchService.updateSearch(saveData, isUpdate, correlationId, options); })
-                .then(() => { return self.documentService.documentsToSearch(saveData, correlationId, options); })
+             //   .then(() => { return self.searchService.updateSearch(saveData, isUpdate, correlationId, options); })
+              //  .then(() => { return self.documentService.documentsToSearch(saveData, correlationId, options); })
                // .then(() => { return self.commentSearchService.commentsToSearch(saveData, correlationId, options); })
-                .then(() => { return self.threadSearchService.toSearch(saveData, correlationId, options); })
+              //  .then(() => { return self.threadSearchService.toSearch(saveData, correlationId, options); })
                 // resolve promise
                 .then(() => { resolve(saveData) }) // resolve promise
 
