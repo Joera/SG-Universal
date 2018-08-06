6b2ea1bd867fb9c5c60a67e56c5db58ad58808c6
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-08-06 12:45:44 +0200

    remove logs

--- services/search.service.js
+++ services/search.service.js
@@ -78,9 +78,9 @@ class SearchService {
 
                 // trim comments
                 let algoliaData = JSON.parse(JSON.stringify(data));
-                if(algoliaData.comments && algoliaData.comments.length > 0) {
-                    algoliaData.comments = algoliaData.comments.slice(0,1);
-                }
+                // if(algoliaData.comments && algoliaData.comments.length > 0) {
+                //     algoliaData.comments = algoliaData.comments.slice(0,1);
+                // }
                 if(algoliaData.interaction && algoliaData.interaction.comments && algoliaData.interaction.comments.length > 0) {
                     algoliaData.interaction.comments = algoliaData.interaction.comments.slice(0,1);
                 }
