5e8fef9c69208b43cc413d6b3e5ac31910bc08c4
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-08-06 17:46:46 +0200

    remove logs

--- controllers/sync.controller.js
+++ controllers/sync.controller.js
@@ -38,6 +38,7 @@ class SyncController {
             const correlationId = uuidv4(); // set correlation id for debugging the process chain
             self.cmsConnector.getPages(page,correlationId)
                 .then((pages) => {
+                    logger.info(pages.length);
                     return new Promise((res, rej) => { cmsPages = pages; res({}); })}) // save pages received from cms api for later use
 
                 // delete pages
