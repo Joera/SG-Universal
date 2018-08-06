ecf584051d87a27614d1c484ac2ba57da5ef590b
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-08-06 11:59:54 +0200

    remove logs

--- services/search.comment.service.js
+++ services/search.comment.service.js
@@ -36,26 +36,27 @@ class CommentSearchService {
             if(data.interaction.nested_comments && data.interaction.nested_comments.length > 0) {
 
                 self._createSnippetData(data,correlationId)
-                    .then((threads) => {
+                    .then((comments) => {
 
-                        data.threads = [];
+                        data.comments = comments;
 
-                        return new Promise((res, rej) => {
-                            for (let i = 0; i < threads.length; i++) {
-                                let threadObject = {};
-
-                                if (threads[i] !== null) {
-                                    threadObject.objectID = threads[i].id;
-                                    threadObject.date = threads[i].date;
-                                    threadObject.type = 'comments';
-                                    threadObject.snippetData = threads[i];
-                                    threadObject.comments = threads[i].comments;
-                                    data.threads.push(threadObject);
-                                }
-                                
-                            }
-                            res(data);
-                        });
+                        // return new Promise((res, rej) => {
+                        //     for (let i = 0; i < comments.length; i++) {
+                        //         let threadObject = {};
+                        //
+                        //         if (threads[i] !== null) {
+                        //             threadObject.objectID = threads[i].id;
+                        //             threadObject.date = threads[i].date;
+                        //             threadObject.type = 'comments';
+                        //             threadObject.snippetData = threads[i];
+                        //             threadObject.comments = threads[i].comments;
+                        //             data.threads.push(threadObject);
+                        //         }
+                        //
+                        //     }
+                        //     res(data);
+                        // });
+                        resolve(data);
                     })
                     .then((data) => { return self._renderSnippets(data,correlationId); })
                     .then((snippets) => {
@@ -86,30 +87,55 @@ class CommentSearchService {
 
         if(data.interaction && data.interaction.nested_comments && data.interaction.nested_comments.length > 0) {
 
+            // alle comments los .. maar thread insluiten
 
-            return Promise.all(data.interaction.nested_comments.map(function (thread) {
+            let comments = [];
 
-                return new Promise(function (resolve, reject) {
+            data.interaction.nested_comments.forEach ( (thread)  => {
 
-                    if(thread && thread[0] && thread[0] !== null) {
+                thread.forEach ( (comment) => {
 
-                        var renderConfig = {
-                            id: thread[0].id,
-                            author: thread[0].name,
-                            content: thread[0].content,
-                            date: thread[0].date,
-                            comments: thread,
-                            reply_count: thread.length - 1,
-                            url: data.url + '#dialoog'
+                    var renderConfig = {
+                        id: comment.id,
+                        author: comment.name,
+                        content: comment.content,
+                        date: comment.date,
+                        thread: thread,
+                        reply_count: thread.comments.length - 1,
+                        url: data.url + '#dialoog'
 
-                        }
-
-                        thread = renderConfig;
                     }
 
-                    resolve(thread);
-                });
-            }))
+                    comments.push(renderConfig);
+                })
+            }
+
+            resolve(comments);
+
+
+            // return Promise.all(data.interaction.nested_comments.map(function (thread) {
+            //
+            //     return new Promise(function (resolve, reject) {
+            //
+            //         if(thread && thread[0] && thread[0] !== null) {
+            //
+            //             var renderConfig = {
+            //                 id: thread[0].id,
+            //                 author: thread[0].name,
+            //                 content: thread[0].content,
+            //                 date: thread[0].date,
+            //                 comments: thread,
+            //                 reply_count: thread.length - 1,
+            //                 url: data.url + '#dialoog'
+            //
+            //             }
+            //
+            //             thread = renderConfig;
+            //         }
+            //
+            //         resolve(thread);
+            //     });
+            // }))
 
         } else {
             data.comments = null;
@@ -120,9 +146,9 @@ class CommentSearchService {
 
         let self = this;
 
-        if(data.threads && data.threads.length > 0) {
-            return Promise.all(data.threads.map(function (thread) {
-                return self.templateService.render('search-snippet','thread.handlebars', thread.snippetData, correlationId);
+        if(data.comments && data.comments.length > 0) {
+            return Promise.all(data.comments.map(function (comment) {
+                return self.templateService.render('search-snippet','thread.handlebars', comment.snippetData, correlationId);
             }))
         }
 
