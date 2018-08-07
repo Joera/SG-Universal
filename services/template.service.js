eeb6c22d00cc1cddcf1cb77fe35b3da18c6ee940
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-08-07 15:41:17 +0200

    a

--- services/template.service.js
+++ services/template.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Promise = require('bluebird');
-const fs = require('fs');
+const fs = require('graceful-fs');
 const minify = require('html-minifier').minify;
 const handlebars = require('handlebars');
 const logger = require('../services/logger.service');
