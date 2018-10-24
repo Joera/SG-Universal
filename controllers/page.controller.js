92e4e6ddb7d8c5411701f766be8b44bbd8552139
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-10-24 12:23:35 +0200

    variable snippet

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -240,9 +240,9 @@ class PageController {
                 .then(() => { return self.pagePersistence.save(saveData, correlationId, options) }) // save page to database
                 .then(() => { return self.datasetService.saveDataset(saveData,persistent_path) }) // save page to database
                 // // only update search if search snippet is rendered. if searchSnippet property on data object is undefined or an empty string search will NOT be updated
-                .then(() => { return self.searchService.updateSearch(saveData, isUpdate, correlationId, options); })
-                .then(() => { return self.documentService.documentsToSearch(saveData, correlationId, options); })
-                .then(() => { return self.commentSearchService.commentsToSearch(saveData, correlationId, options); })
+                // .then(() => { return self.searchService.updateSearch(saveData, isUpdate, correlationId, options); })
+                // .then(() => { return self.documentService.documentsToSearch(saveData, correlationId, options); })
+                // .then(() => { return self.commentSearchService.commentsToSearch(saveData, correlationId, options); })
                 // resolve promise
                 .then(() => { resolve(saveData) }) // resolve promise
 
