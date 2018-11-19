9a88ebbbcb2c38e43b7425943fc6aff2da53418f
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-19 13:48:38 +0100

    ddd

--- services/calendar.service.js
+++ services/calendar.service.js
@@ -65,7 +65,7 @@ class CalendarService {
 
                 extraActivity = clone(data);
                 logger.info(data.calendar.recurrentDates[i]);
-                extraActivity.calendar.startDate = data.calendar.recurrentDates[i];
+                extraActivity.calendar.startDate = new Date(data.calendar.recurrentDates[i].date);
                 extraActivity.objectID = data._id + '-' + i;
                 extraActivities.push(extraActivity);
             }
