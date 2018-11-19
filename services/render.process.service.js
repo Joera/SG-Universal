dc37c6ce81d2d38fd882518c30471077f7b4677b
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-19 10:23:26 +0100

    waaa

--- services/render.process.service.js
+++ services/render.process.service.js
@@ -141,9 +141,9 @@ class RenderProcessService {
                 .then(() => { return templateDefinition.preRender(path, data, correlationId) }) // execute the pre render hook
                 .then((templateData) => { return self.templateService.render(name, template, templateData, correlationId) }) // render template
                 .then((html) => { return new Promise((res, rej) => { templateHtml = html; res({}); }) }) // set html for later use
-                .then(() => { logger.info('ypyoyoyoyoyoyoyoyoyoy'); return templateDefinition.postRender(templateHtml, path, data, correlationId) }) // execute the post render hook
+                .then(() => { logger.info(templateDefinition); return templateDefinition.postRender(templateHtml, path, data, correlationId) }) // execute the post render hook
                 // write template file
-                .then(() => { return self.fileSystemConnector.createDirectory(path, correlationId) }) // save template directory
+                .then(() => { logger.info('4'); return self.fileSystemConnector.createDirectory(path, correlationId) }) // save template directory
                 .then(() => { return self.fileSystemConnector.writeTemplateFile(path, templateHtml, correlationId) }) // write template file
 
                 //
