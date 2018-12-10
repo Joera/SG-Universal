411ec34dc5aa07a409da0d0368ff6a63a1d50340
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-10 21:22:20 +0100

    cors

--- config/express.js
+++ config/express.js
@@ -20,6 +20,26 @@ app.use(function (error, req, res, next) { // catch bodyParser syntax error and 
     }
 });
 
+/********* DEZE HEADERS STRAKS MOGELIJK VERWIJDEREN *********/
+// add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 
 // enable CORS - Cross Origin Resource Sharing
