612a1bad3779c6cca95530005a489a366565ec81
Joera <joeramulders@gmail.com>
2020-02-18 14:36:27 +0100

    trim arts for index

--- services/search.service.js
+++ services/search.service.js
@@ -138,7 +138,7 @@ class SearchService {
                     // algoliaObject.sections = Object.entries(algoliaObject.sections).slice(0,2).map(entry => entry[1]);
                 }
 
-            algoliaObject.sections = null;
+                algoliaObject.sections = null;
 
               //   algoliaObject.excerpt = null;
                 algoliaObject.main_image = null;
