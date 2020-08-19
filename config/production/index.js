c9df9cc93854299f9878fa59b9d3661e772933a2
Joera <joeramulders@gmail.com>
2020-08-19 19:02:33 +0200

    config in repo

---
+++ config/production/index.js
@@ -0,0 +1,16 @@
+const devConfig = {
+    env: 'development',
+    dist: '/var/www/html',
+    db: 'mongodb://shared_mongo',
+    port: 3713,
+    wordpressUrl: 'https://eureka-wp.publikaan.nl',
+    wordpressApiPath: 'wp-json/wp/v2',
+    algoliaIndexNamePrefix: 'eureka-staging_',
+    algoliaApplicationId: 'RLZJT7BFZT',
+    algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
+    baseUrl: 'https://eureka.publikaan.nl',
+    templateNameKey: 'type',
+    renderTasks: [],
+};
+
+module.exports = devConfig;
