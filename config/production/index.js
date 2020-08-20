22ad05b414b0b47f1b80e0640201fd41771918e0
Joera <joeramulders@gmail.com>
2020-08-20 13:23:03 +0200

    conf

--- config/production/index.js
+++ config/production/index.js
@@ -10,7 +10,7 @@ const devConfig = {
     algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
     baseUrl: 'https://eureka.publikaan.nl',
     templateNameKey: 'type',
-    renderTasks: [],
+    renderTasks: ['searchPosts','dataset','searchDocuments'],
 };
 
 module.exports = devConfig;
