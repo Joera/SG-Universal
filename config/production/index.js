ce750a8f680cdff46368888dc80a7fc174afbafb
Joera <joeramulders@gmail.com>
2020-08-30 10:56:04 +0200

    changed domain

--- config/production/index.js
+++ config/production/index.js
@@ -8,7 +8,7 @@ const devConfig = {
     algoliaIndexNamePrefix: 'eureka-staging_',
     algoliaApplicationId: 'RLZJT7BFZT',
     algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
-    baseUrl: 'https://eurekarail.net/',
+    baseUrl: 'https://eurekarail.net',
     templateNameKey: 'type',
     renderTasks: ['searchPosts','dataset','searchDocuments'],
 };
