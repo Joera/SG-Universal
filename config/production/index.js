457955c89e0f627d5133973245f9c67fde550164
Joera <joeramulders@gmail.com>
2020-08-30 09:11:31 +0200

    changed domain

--- config/production/index.js
+++ config/production/index.js
@@ -1,14 +1,14 @@
 const devConfig = {
-    env: 'development',
+    env: 'production',
     dist: '/var/www/html',
     db: 'mongodb://shared_mongo/eurekarail',
     port: 3713,
-    wordpressUrl: 'https://eureka-wp.publikaan.nl',
+    wordpressUrl: 'https://wp.eurekarail.net',
     wordpressApiPath: 'api/get_posts',
     algoliaIndexNamePrefix: 'eureka-staging_',
     algoliaApplicationId: 'RLZJT7BFZT',
     algoliaApiKey: 'f0dc506dd42b55db93e55d28c3e667b5',
-    baseUrl: 'https://eureka.publikaan.nl',
+    baseUrl: 'https://eurekarail.net/',
     templateNameKey: 'type',
     renderTasks: ['searchPosts','dataset','searchDocuments'],
 };
